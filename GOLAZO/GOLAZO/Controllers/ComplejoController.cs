8e8bd714c71e331cdc8e49a09bd1937a7a60af42
JOSUE ESTEBAN FLORES PAZ <josue.flores02@inacapmail.cl>
2020-06-27 21:58:02 -0400

    index Complejos

---
+++ GOLAZO/GOLAZO/Controllers/ComplejoController.cs
@@ -0,0 +1,22 @@
+﻿using GOLAZO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GOLAZO.Controllers
+{
+    public class ComplejoController : Controller
+    {
+        // GET: Complejo
+        public ActionResult Index()
+        {
+            GolazoContext db = new GolazoContext();
+
+            List<COMPLEJO> complejos = db.COMPLEJO.ToList();
+
+            return View(complejos);
+        }
+    }
+}
